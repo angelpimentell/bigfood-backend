5b38ae7ad76287896ce8cf5720c88088ad256bfc
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-12-18 06:49:02 -0400

    [ADD] Bill model, factory, migration and controller

---
+++ app/Models/Restaurant/Bill.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Restaurant;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bill extends Model
+{
+    use HasFactory;
+}
