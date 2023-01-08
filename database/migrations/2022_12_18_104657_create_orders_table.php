a86e069e2f8b6a091a27afb7598d4f935b86da77
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-01-08 18:11:37 -0400

    [REMOVE] Unnecessary field in order migration

--- database/migrations/2022_12_18_104657_create_orders_table.php
+++ database/migrations/2022_12_18_104657_create_orders_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(\App\Models\Restaurant\Table::class);
-            $table->foreignIdFor(\App\Models\Restaurant\Bill::class);
             $table->timestamps();
         });
     }
